d41d9eb747cdd4070a5542b04b8e0021d74b5a33
Victor Jared Onato <jakewolfman131@gmail.com>
2025-11-30 16:42:02 +0800

    third commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    HIBP_URL: "https://api.pwnedpasswords.com/range/",
-    API_NINJA_URL: "https://api.api-ninjas.com/v1/passwordgenerator?length="
-}
+  HIBP_URL: "https://api.pwnedpasswords.com/range/",
+  API_NINJA_URL: "https://api.api-ninjas.com/v1/passwordgenerator?length=",
+};
