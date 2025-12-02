20a962fbae797f8ded934748dd232bd6fd81e961
Victor Jared Onato <jakewolfman131@gmail.com>
2025-12-02 11:17:50 +0800

    Added the generate index to vite config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         checker: resolve(__dirname, "src/checker/index.html"),
+        generate: resolve(__dirname, "src/generate/index.html"),
       },
     },
   },
